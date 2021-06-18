fa65f906d0226a832b35b60988f6194375cc6ed6
root <root@kali>
2021-06-18 00:04:23 -0500

    LlP_0x00_My_First_Project_in_C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
-main() {puts("\"Programming is like building a multilingual puzzle"); return 0; }
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
+}
 
