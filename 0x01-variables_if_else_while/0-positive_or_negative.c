1573d347c80009c2ce1295a5e3c68451d83c9320
root <root@kali>
2021-06-18 13:48:11 -0500

    LlP_0x01_My_First_if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n < 0)
+	{
+		printf("%i is negative", n);
+	}
+	else if (n > 0)
+	{
+		printf("%i is positive", n);
+	}
+	else
+	{
+		printf("%i is zero", n);
+	}
+	return (0);
+}
