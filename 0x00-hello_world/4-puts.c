0e13b28c3df60785d9d16e1830d2d19c3bbc2e5a
root <root@kali>
2021-06-18 00:06:59 -0500

    LlP_0x00_My_First_Project_in_C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - main function
- * Return: 0
- */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+	return (0);
 }
-
