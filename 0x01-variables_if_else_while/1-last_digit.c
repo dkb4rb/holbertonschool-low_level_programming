40cca382a4659418a6f380ca77a9188d72661d77
root <root@kali>
2021-06-19 00:36:26 -0500

    LlP_0x01_My_second_if_with_lengt

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**Entry point - MAIN
+ *  
+ * output - return (0)
+ */
+
+int main()
+{
+    int n, is;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    is = n % 10;
+    if (is > 5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, is);
+    }
+    else if (is == 0)
+    {
+        printf("Last digit of %d is %d and is 0\n", n, is);
+    }
+    else
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, is);
+    }
+    return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * entry main
+ *
+ * return (0);
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
