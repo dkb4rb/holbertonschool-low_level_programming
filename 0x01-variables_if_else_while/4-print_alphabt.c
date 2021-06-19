0a8c970ec18b38824bd694e7e7ce10d93680b268
root <root@kali>
2021-06-19 02:03:51 -0500

    LlP_0x01_My_second_if_with_lengt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,14 @@
 
 int main(void)
 {
-	int i;
+	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(i);
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
