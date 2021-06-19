c5d7a5dc2271128f3448619ee9aa0d58a0135f08
root <root@kali>
2021-06-19 01:16:34 -0500

    LlP_0x01_My_second_if_with_lengt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("%i is negative", n);
+		printf("%d is negative", n);
 	}
 	else if (n > 0)
 	{
-		printf("%i is positive", n);
+		printf("%d is positive", n);
 	}
 	else
 	{
-		printf("%i is zero", n);
+		printf("%d is zero", n);
 	}
 	return (0);
 }
