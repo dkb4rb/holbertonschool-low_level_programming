ccc63266eaa1a8d7a150b01e044e247858f9958e
root <root@kali>
2021-06-19 01:53:53 -0500

    LlP_0x01_My_second_if_with_lengt

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 	int i, c;
-	for (char i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
 
-	for (char c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 
 		putchar(toupper(c));
