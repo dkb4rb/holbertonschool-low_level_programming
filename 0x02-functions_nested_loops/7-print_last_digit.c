5edf4cb23bb17cbb8e91d60fe9357429cde84ec8
root <root@kali>
2021-06-22 18:24:28 -0500

    LlP_0x02_Functions_absolute_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,18 @@
 #include "holberton.h"
 
 /**
- * function that prints the last digit of a number.
- * @brief 
- * 
+ * print_last_digit - function that prints the last digit of a number.
+ * @d:the value
+ * Return: value of the d
  */
 
 int print_last_digit(int d)
 {
-    
-    d = d % 10;
-    if (d < 0){
-
-        d = (d * (-1));
-    }
-    
-    _putchar(d + '0');
-    return(d);
-
-
+d = d % 10;
+if (d < 0)
+{
+d = (d * (-1));
+}
+_putchar(d + '0');
+return (d);
 }
