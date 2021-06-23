f549ed23b8723a014be552b977160f31dd15fe43
root <root@kali>
2021-06-22 19:05:52 -0500

    LlP_0x02_Functions_Hour_of_Jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * jack_bauer - function that prints every minute of the day of Jack Bauer.
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+int min;
+int hour;
+while (hour <= 23)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar(hour / 10 + '0');
+_putchar(hour % 10 + '0');
+_putchar(':');
+_putchar(min / 10 + '0');
+_putchar(min % 10 + '0');
+_putchar('\n');
+}
+hour++;
+}
+}
