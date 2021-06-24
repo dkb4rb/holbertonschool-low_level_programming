61d0e8b2b0a4ee96fc633112b28a1c094e615d1c
root <root@kali>
2021-06-24 17:47:17 -0500

    LlP_0x04_More_Fucntions_multiplies_two_inteigers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - function that multiplies two integers.
+ * @a: value 1
+ * @b: value 2
+ * Return: the variable mult on save result.
+ */
+
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return (res);
+}
