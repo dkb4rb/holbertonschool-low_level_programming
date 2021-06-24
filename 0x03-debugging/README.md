d32d701737662ca81bbb5a70fcfad4e2bf7fab66
root <root@kali>
2021-06-23 20:49:20 -0500

    LlP_0x03_Debugger_int_i

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+this is repositori,this allign the debuggin procesiog.
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+void positive_or_negative(int i)
+{
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
