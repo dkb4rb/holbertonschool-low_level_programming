80979ce774980a473aa984005538dd69092281bc
root <root@kali>
2021-06-24 16:57:36 -0500

    LlP_0x03_More_Fuctions_Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+int _isupper(int c)
+{
+    if (c >= 'A' && c > 90 )
+    {
+        return (0);
+    } else {
+        return (1);
+    }
+    
+}
