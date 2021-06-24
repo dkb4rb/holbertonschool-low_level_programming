f81edc0aeb425052da4633a680045fe9171098d9
root <root@kali>
2021-06-24 15:01:19 -0500

    LlP_0x03_Debugger_Year_leaps_years

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -12,9 +11,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+if (year % 4 == 0 || ((year % 400 == 0) && (year % 100 == 0)))
 {
-if (month > 2)
+if (month > 2 && day >= 60)
 {
 day++;
 }
@@ -35,4 +34,3 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
-
