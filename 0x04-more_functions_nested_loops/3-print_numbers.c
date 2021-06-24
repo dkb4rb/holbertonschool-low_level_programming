6b989383365da55489a994637461ceaa1af30ed3
root <root@kali>
2021-06-24 17:55:08 -0500

    LlP_0x04_More_Fucntions__putchar_numbers_naturals

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * @brief 
+ * 
+ */
+
+void print_numbers(void)
+{
+    int numbers = 48;
+    while (numbers <= 57)
+    {
+        _putchar(numbers);
+        numbers++;
+    }
+    _putchar('\n');
+        
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -6,5 +6,8 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+
 
 #endif /* HOLBERTON_H */
