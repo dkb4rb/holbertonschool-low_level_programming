8eef2e3b12ecad982e9f3bc93dba20c8068d6934
root <root@kali>
2021-06-24 21:16:56 -0500

    LlP_0x04_More_Fucntions_Buzz_fizz_test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+**/
+
+int main(void)
+{
+	int Bz= 1 ;
+
+	while (Bz <= 100)
+	{
+		if (((Bz % 3) == 0) && ((Bz % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((Bz % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((Bz % 5) == 0)
+		{
+			if (Bz != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%i ", Bz);
+		}
+		Bz++;
+	}
+	printf("\n");
+	return (0);
+}
