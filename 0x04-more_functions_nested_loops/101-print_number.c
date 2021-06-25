858081c8faf48bddb5b690d8f171d6365b42c88a
root <root@kali>
2021-06-25 00:01:28 -0500

    LlP_0x04_More_Fucntions_Advanced_2

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+   * print_number - function that prints an integer.
+    * @n: number
+     * Return: void
+     **/
+
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	}
+	else
+	{
+		x = n;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar(x % 10 + '0');
+}
