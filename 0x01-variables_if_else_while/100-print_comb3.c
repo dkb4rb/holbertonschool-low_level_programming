e56ec97328a2fd8336775d41cdb2d1fd3a061643
DKBARB10 <84897255+DKBARB10@users.noreply.github.com>
2021-06-26 20:28:18 -0500

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,48 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
-
+  * main - Prints combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-	int init;
+	int c, i;
 
-	for (init = 1; init <= 89; init++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (
-			(
-				(init != 10) && (init != 11)) &&
-			(((init != 20) && (init != 21) && (init != 22))) &&
-			(((init != 30) && (init != 31) && (init != 32 && init != 33))) &&
-			(((init != 40) && (init != 41) &&
-			(init != 42) && (init != 43) && (init != 44))) &&
-			(((init != 50) && (init != 51) && (init != 52) &&
-			(init != 53) && (init != 54) && (init != 55))) &&
-			(((init != 60) && (init != 61) && (init != 62) &&
-			(init != 63) && (init != 64) && (init != 65) &&
-			(init != 66))) && (((init != 70) && (init != 71) &&
-			(init != 72) && (init != 73) && (init != 74) && (init != 75) &&
-			(init != 76) && (init != 77))) && (((init != 80) && (init != 81) &&
-			(init != 82) && (init != 83) && (init != 84) && (init != 85) &&
-			(init != 86) && (init != 87) && (init != 88)))
-
-		)
+		for (i = '0'; i <= '9'; i++)
 		{
-			putchar(((init / 10) + '0'));
-			putchar(((init % 10) + '0'));
-
-			if (init != 89)
+			if (c < i)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
