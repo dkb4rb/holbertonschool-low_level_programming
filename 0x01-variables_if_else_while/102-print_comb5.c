58bae7acbb32392d1e19686dee604ae73b56e521
DKBARB10 <jjuannca651@gmail.com>
2021-06-27 22:44:24 -0500

    0x01_if_else_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Prints combination of numbers
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+
+int h,m,i;
+for ( h = 0; h < 99; h++ )
+{
+     
+        for ( m = 1; m < 100; m++)
+        {
+            
+        putchar((h / 10) + '0');
+        putchar((h % 10) + '0');
+        putchar(':');
+        putchar((m / 10) + '0');
+        putchar((m % 10) + '0');
+        putchar('\n');
+        
+      }
+      
+        
+   
+}
+
+    putchar('\n');
+    return (0);
+}
+
