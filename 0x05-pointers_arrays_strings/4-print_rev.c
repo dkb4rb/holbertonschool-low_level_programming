e72a0956879972206abf90a300195899905121ca
DKBARB10 <jjuannca651@gmail.com>
2021-06-28 21:49:38 -0500

    0x05_Pointers_puts_rever

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 
 /**
-* _puts - function that returns the length of a string of str.
-*@str: pointer value
-*Return: c value
+* print_rev - prints a string, in reverse,followed by a new line.
+*@s: pointer value
 */
 
 void print_rev(char *s)
 {
-    int c = 0;
-    while (c[s] != '\0')
-    {
-        c++;
-    }
-    c = c - 1;
-    while (c >= 0)
-    {
-        _putchar(s[c]);
-        c--;
-    }
-    _putchar('\n');
+	int c = 0;
+
+	while (c[s] != '\0')
+	{
+		c++;
+	}
+	c = c - 1;
+	while (c >= 0)
+	{
+		_putchar(s[c]);
+		c--;
+	}
+	_putchar('\n');
 }
