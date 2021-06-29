e2af52507098e42a38fd13c69a15592fa5d2a4ad
DKBARB10 <jjuannca651@gmail.com>
2021-06-28 20:44:12 -0500

    0x05_Pointers_change_two_integers

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char str;
+    char *str;
     int len;
 
     str = "Holberton!";
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,10 +9,9 @@
 int _strlen(char *s)
 {
     int c = 0;
-    while (*s != '\0')
+    while (s[c] != '\0')
     {
         c++;
     }
-
     return (c);
 }
