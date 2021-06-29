47e97f979a3d796989f175134936c6d3c2f4cac1
DKBARB10 <jjuannca651@gmail.com>
2021-06-28 20:22:25 -0500

    0x05_Pointers_change_two_integers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - Reset value and updates the value it points to to 98.
+*@n: pointer value
+*/
+
+int _strlen(char *s)
+{
+    int c = 0;
+    while (*s != '\0')
+    {
+        c++;
+    }
+
+    return (c);
+}
