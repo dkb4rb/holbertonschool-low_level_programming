d59985152252a60173fe48218a3c0b1704cf207a
DKBARB10 <jjuannca651@gmail.com>
2021-06-29 22:41:02 -0500

    0x05_Pointers_putchar_up_fiv3_numbers_

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,19 @@
 
 void puts_half(char *str)
 {
-	int i = 0;
+	int i = 0, l = 0, n;
 
-	for (; str[i] != '\0'; i++)
-	{
-		if (i >= 5)
-		{
-			_putchar(str[i]);
-		}
-	}
+	while (str[i++])
+		l++;
+
+	if ((l % 2) == 0)
+		n = l / 2;
+
+	else
+		n = (l + 1) / 2;
+
+	for (i = n; i < l; i++)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
