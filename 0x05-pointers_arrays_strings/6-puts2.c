26bf3ead7b033e600dbae98d879c48c862ea53a7
DKBARB10 <jjuannca651@gmail.com>
2021-06-29 20:03:48 -0500

    0x05_Pointers_putchar_up_two_dates

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * 
- * @:
- * */
+ *puts2 - print values od two int.
+ *@str: is pointer value
+ */
 
 void puts2(char *str)
 {
 
-    int c = 0;
+	int c = 0;
 
-    while (c < 8)
-    {
-        c++;
-        putchar(*str);
-        str = str + 2;
-    }
+	while (c < 8)
+	{
+		c++;
+		putchar(*str);
+		str = str + 2;
+	}
 }
