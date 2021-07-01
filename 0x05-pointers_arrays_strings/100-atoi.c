c22440b7f90d76cadeb6eab11c790ebc203ed04a
DKBARB10 <jjuannca651@gmail.com>
2021-07-01 06:40:18 -0500

    0x05_Pointers_else_numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,27 @@
 #include <stdlib.h>
 #include "holberton.h"
 
+/**
+* _atoi - Function that calculate a numbers, only nombers in a string
+* @s: String that evaluates
+* Return: The integer conversion
+*/
+
 int _atoi(char *s)
 {
+	int sign = 1;
+	unsigned int num = 0;
 
-    int sign = 1;
-    unsigned int num = 0;
+	do {
+		if (*s == '-')
+			sign *= -1;
 
-    do
-    {
-        if (*s == '-')
-            sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-        else if (*s >= '0' && *s <= '9')
-            num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
 
-        else if (num > 0)
-            break;
-
-    } while (*s++);
-    return (num * sign);
+	} while (*s++);
+	return (num * sign);
 }
