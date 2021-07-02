c35bbd40a258ed3a26cc5746ac82469df15782af
DKBARB10 <jjuannca651@gmail.com>
2021-07-01 23:09:22 -0500

    0x06_MOre_Pointers_compares_arrays

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * 
+ *
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		i++;
+	}
+
+	return (str);
+}
