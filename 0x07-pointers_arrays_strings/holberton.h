08937ff258595770d2a941839fd8007947cdbbf9
DKBARB10 <jjuannca651@gmail.com>
2021-07-06 02:57:55 -0500

    0x07_Pointer_and_More_Strcpy

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,5 +5,6 @@
 void *_memset(void *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
 
 #endif /*HOLBERTON_H*/
