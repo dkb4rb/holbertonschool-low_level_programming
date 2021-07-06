80e2b48943ed534ec893771f117f88f4499b2371
DKBARB10 <jjuannca651@gmail.com>
2021-07-05 21:27:15 -0500

    0x07_Pointer_and_More_PointersM

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
+ * _memset - Fills the first n bytes of the memory area
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
