2d7626cd7fdfc485df50942444f1f633ac558e83
DKBARB10 <jjuannca651@gmail.com>
2021-07-06 22:28:50 -0500

    0x06_Mozart_Pointer

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,20 +1,26 @@
 #include "holberton.h"
 
+/**
+*leet - Change text in 1337
+*@p: parameter return
+*Return: p values
+*/
+
 char *leet(char *p)
 {
-    int a, b;
-    char text1[10] = "aAeEoOtTlL";
-    char text2[10] = "4433007711";
+	int a, b;
+	char text1[10] = "aAeEoOtTlL";
+	char text2[10] = "4433007711";
 
-    for (a = 0; p[a]; a++)
-    {
-        for (b = 0; b < 10; b++)
-        {
-            if (p[a] == text1[b])
-            {
-                p[a] = text2[b];
-            }
-        }
-    }
-    return (p);
+	for (a = 0; p[a]; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (p[a] == text1[b])
+			{
+				p[a] = text2[b];
+			}
+		}
+	}
+	return (p);
 }
