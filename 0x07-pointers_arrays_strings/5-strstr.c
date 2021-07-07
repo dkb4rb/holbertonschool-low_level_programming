82423e20222991aff6eebee7467b57eef0b13d31
DKBARB10 <jjuannca651@gmail.com>
2021-07-06 23:24:07 -0500

    0x06_Mozart_Pointer

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,13 +8,28 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i = 0;
-    for (; i < haystack[i]; i++)
-    {
-    }
+    int i;
 
-    if (haystack[i] == needle[i])
-        return (needle);
+    if (*needle == 0)
+        return (haystack);
+
+    while (*haystack)
+    {
+        i = 0;
+        if (haystack[i] == needle[i])
+        {
+            do
+            {
+                if (needle[i + 1] == '\0')
+                {
+                    return (haystack);
+                }
+                i++;
+
+            } while (haystack[i] == needle[i]);
+        }
+        haystack++;
+    }
 
     return ('\0');
 }
