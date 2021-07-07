5f524f8acb897eb5cf24403221c1d2d3d372de42
DKBARB10 <jjuannca651@gmail.com>
2021-07-06 23:40:03 -0500

    0x06_Mozart_Pointer

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -2,10 +2,12 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
+int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 
 #endif /*HOLBERTON_H*/
