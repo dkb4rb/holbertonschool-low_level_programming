bfa76ac9445a931a033d002c883a28946800a9a0
DKBARB10 <jjuannca651@gmail.com>
2021-07-07 21:16:34 -0500

    0x08_Recursion_reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _print_rev_recursion - reverse text
+*@s: value pointer
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
