b598073f2b3298517a826983cc5de7a3439cdf41
DKBARB10 <jjuannca651@gmail.com>
2021-07-07 23:07:05 -0500

    0x08_Recursion_This_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * prime_check - check number if this is prime
+ * @z: checkout factor prime
+ * @d: number factorial
+ * Return: prime_check with factorial number.
+*/
+int prime_check(int z, int d)
+{
+	if (z < 2 || z % d == 0)
+		return (0);
+	else if (z > d / 2)
+		return (1);
+	else
+		return (prime_check(z + 1, d));
+}
+
+/**
+ * is_prime_number - function that returns 1 if the input.
+ * @n: this value pointer
+ * Return: function prime_check
+*/
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (prime_check(2, n));
+}
