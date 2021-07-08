316cee68d8cafcd90353329baacd6b57b61d30ff
DKBARB10 <jjuannca651@gmail.com>
2021-07-07 22:35:25 -0500

    0x08_Recursion_pow_factorial

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ */
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
+}
