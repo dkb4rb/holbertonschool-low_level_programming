b6589955640def39c0dabe6f0e12e5404aa713a5
DKBARB10 <jjuannca651@gmail.com>
2021-07-09 21:48:18 -0500

    0x08_Argc_Argv_mult_my_functions

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,14 +14,13 @@ int main(int argc, char **argv)
 {
 	int v1, v2, rslt;
 
-	v1 = _atoi(argv[1]);
-	v2 = _atoi(argv[2]);
-
 	if (argc != 3)
 	{
 		printf("Error");
 		return (1);
 	}
+	v1 = _atoi(argv[1]);
+	v2 = _atoi(argv[2]);
 	rslt = mult(v1, v2);
 
 	printf("%d\n", rslt);
