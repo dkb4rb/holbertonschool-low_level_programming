fba275eb86af4adcd5724881b0a3274b55f40d96
DKBARB10 <jjuannca651@gmail.com>
2021-07-09 20:43:21 -0500

    0x08_Argc_Argv_arguments_upper_line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main -  program that prints its name, followed by a new line.
+ *@argc: argument count
+ *@argv: argument value
+ *Return: null value
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
