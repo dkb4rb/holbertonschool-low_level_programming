b3f083574d5f2a4f91e29c7ac8a27938e5fc47a8
DKBARB10 <jjuannca651@gmail.com>
2021-07-09 20:35:45 -0500

    0x08_Argc_Argv_unasigned_atribute

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main -  program that prints its name, followed by a new line.
+ *@argc: argument count
+ *@argv: argument value
+ *Return: null value
+ */
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+
+	printf("%d\n", argc);
+
+	return (0);
+}
