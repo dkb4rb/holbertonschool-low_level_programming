a346375e05290367fb705dc53fffd2e65b80f538
DKBARB10 <jjuannca651@gmail.com>
2021-07-12 03:17:15 -0500

    0x08_error_4

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,8 @@
-List of Exercises Functions
+<h1 class="gap">0x09. C - Static libraries</h1>
+
+<h2>Learning Objectives</h2>
+
+<ul>
+<li>What is a static library, how does it work, how to create one, and how to use it</li>
+<li>Basic usage of <code>ar</code>, <code>ranlib</code>, <code>nm</code></li>
+</ul>
--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,5 @@
-#ifndef _holberton
-#define _holberton
-
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
