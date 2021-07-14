7db5c7401323ea1da8e1df9a3636be9d2a4a737b
DKBARB10 <jjuannca651@gmail.com>
2021-07-14 07:23:41 -0500

    0x0B_Malloc_free_memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,16 +6,17 @@
  * init - function that initialize.
  * @argv: Argument Vector
  * @width: Width of the array 2d
+ * @counter: Counter of the lenght
  * Return: If size == 0 or the function fails - NULL.
  *         Otherwise - a pointer to the array.
  */
 
-void init(int **argv, int width)
+void init(int **argv, int width, int counter)
 {
 
 	int copywidth = 0;
 
-	while (*argv)
+	while (counter--)
 	{
 
 		if (copywidth < width)
@@ -33,6 +34,27 @@ void init(int **argv, int width)
 }
 
 /**
+ * Free - function that free memories.
+ * @argv: Argument Vector
+ * @counter: Counter of the lenght
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
+ */
+
+void Free(int **argv, int counter)
+{
+	int **temp = argv;
+
+	while (counter--)
+	{
+
+		free(*argv);
+		argv++;
+	}
+	free(temp);
+}
+
+/**
  * alloc_grid - function that converse a grid of aa 2 dimensional pointer.
  * @width: Width of the array 2d
  * @height: Height of the array 2d
@@ -45,26 +67,36 @@ int **alloc_grid(int width, int height)
 
 	int **argv;
 	int **argv1;
+	int counter = 0;
 
-	argv = (int **)malloc(sizeof(int *) * (height + 1));
-	argv[height] = NULL;
+	if (width <= 0 && height <= 0)
+	{
+		return (NULL);
+	}
+
+	argv = (int **)malloc(sizeof(int *) * (height));
 	argv1 = argv;
+	if (!argv)
+	{
+		free(argv);
+		return (NULL);
+	}
 
-	for (; height; --height)
+	for (; counter < height; counter++)
 	{
 
 		*argv = (int *)malloc(sizeof(int) * width);
 
-		if (*argv == NULL)
+		if (!*argv)
 		{
-
+			Free(argv1, counter);
 			return (NULL);
 		}
 
 		argv++;
 	}
 
-	init(argv1, width);
+	init(argv1, width, height);
 
 	return (argv1);
 }
