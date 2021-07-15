284ab8d4ce581201aedadbeff8b3241e409485ef
DKBARB10 <jjuannca651@gmail.com>
2021-07-15 13:27:11 -0500

    0x0C_Malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-malloc_check.c
+ * Auth: Juan Duque
+ * Medellin(Ant)
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Entry point
+ *@b: string we need to duplicate
+ * Return: a pointer to the allocate memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = NULL;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void *malloc_checked(unsigned int b);
+
+#endif
