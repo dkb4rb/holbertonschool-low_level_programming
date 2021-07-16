8b9a1c8c9ab85dffb482189ad460194459b379c6
DKBARB10 <jjuannca651@gmail.com>
2021-07-16 13:00:55 -0500

    0x0B_Malloc_More_Malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,10 +16,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	if (!ptr)
+	{
 		ptr = malloc(new_size);
-
+	}
 	if (new_size == old_size)
+	{
 		return (ptr);
+	}
 
 	free(ptr);
 	ptr = malloc(new_size);
