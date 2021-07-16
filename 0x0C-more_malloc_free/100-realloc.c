49485fb34d9ee6a3480347e33fb25730290276aa
DKBARB10 <84897255+DKBARB10@users.noreply.github.com>
2021-07-16 13:30:10 -0500

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,30 +10,37 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (!ptr)
-	{
-		void *Rptr = malloc(new_size);
-		return Rptr;
-	}
-	else if (!new_size && ptr)
-	{
-		free(ptr);
-	}
-	else if (new_size > old_size)
-	{
-		void *Rptr = malloc(old_size + new_size);
-		if (!Rptr)
-		{
-			free(ptr);
-			ptr = NULL;
-			return Rptr;
-		}
-		ptr = Rptr;
-		return Rptr;
-	}
-	else if (new_size == old_size)
-	{
-		return (ptr);
-	}
-	return NULL;
+        void *new_ptr;
+        char *ptr_copy, *filler;
+        unsigned int index;
+        if (new_size == old_size)
+                return (ptr);
+        if (ptr == NULL)
+        {
+                new_ptr = malloc(new_size);
+                if (new_ptr == NULL)
+                {
+                        return (NULL);
+                }
+                return (new_ptr);
+        }
+        if (new_size == 0 && ptr != NULL)
+        {
+                free(ptr);
+                return (NULL);
+        }
+        ptr_copy = ptr;
+        new_ptr = malloc(sizeof(*ptr_copy) * new_size);
+        if (new_ptr == NULL)
+        {
+                free(ptr);
+                return (NULL);
+        }
+        filler = new_ptr;
+        for (index = 0; index < old_size && index < new_size; index++)
+        {
+                filler[index] = *ptr_copy++;
+        }
+        free(ptr);
+        return (new_ptr);
 }
