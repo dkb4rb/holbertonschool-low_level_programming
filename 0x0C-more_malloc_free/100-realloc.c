11f2350170b54af959b4fda651528056b8d2f0ae
DKBARB10 <84897255+DKBARB10@users.noreply.github.com>
2021-07-16 13:53:38 -0500

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,37 +10,20 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-        void *new_ptr;
-        char *ptr_copy, *filler;
-        unsigned int index;
-        if (new_size == old_size)
-                return (ptr);
-        if (ptr == NULL)
-        {
-                new_ptr = malloc(new_size);
-                if (new_ptr == NULL)
-                {
-                        return (NULL);
-                }
-                return (new_ptr);
-        }
-        if (new_size == 0 && ptr != NULL)
-        {
-                free(ptr);
-                return (NULL);
-        }
-        ptr_copy = ptr;
-        new_ptr = malloc(sizeof(*ptr_copy) * new_size);
-        if (new_ptr == NULL)
-        {
-                free(ptr);
-                return (NULL);
-        }
-        filler = new_ptr;
-        for (index = 0; index < old_size && index < new_size; index++)
-        {
-                filler[index] = *ptr_copy++;
-        }
+    if (new_size == 0 && ptr != NULL)
+    {
         free(ptr);
-        return (new_ptr);
+        return (NULL);
+    }
+
+    if (ptr == NULL)
+        ptr = malloc(new_size);
+
+    if (new_size == old_size)
+        return (ptr);
+
+    free(ptr);
+    ptr = malloc(new_size);
+
+    return (ptr);
 }
