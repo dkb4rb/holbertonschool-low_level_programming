1c7989269c4109e3fe50e1f38313f3e085cda073
DKBARB10 <jjuannca651@gmail.com>
2021-07-21 22:20:18 -0500

    0x0F_functions to pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%')
+	if (argv[2][0] != '+' && argv[2][0] != '-' &&
+	    argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%')
 	{
 		printf("Error\n");
 		exit(99);
