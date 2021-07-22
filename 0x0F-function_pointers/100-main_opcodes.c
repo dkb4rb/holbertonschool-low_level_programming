351294f0befe23073ba6b8a7e942a07b8833b60e
DKBARB10 <84897255+DKBARB10@users.noreply.github.com>
2021-07-21 22:24:49 -0500

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,24 +1,40 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - searchs for an integer
- *@argc: number of arguments
- *@argv: array with arguments
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: no return
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
+	char *opc = (char *) main;
+	int i, nbytes;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
 	return (0);
 }
