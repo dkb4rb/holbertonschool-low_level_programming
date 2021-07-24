57d480ba13efc431f953bb143b05582b742dcfaa
DKBARB10 <84897255+DKBARB10@users.noreply.github.com>
2021-07-23 23:41:17 -0500

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,83 +1,92 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
-/**
- * _charp - prints a character
- * @list: argument pointer variable, character to print
- */
-void _charp(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-/**
- * _intp - prints an integer
- * @list: argument pointer variable, integer to print
- */
-void _intp(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-/**
- * _floatp - prints a float
- * @list: argument pointer variable, float to print
- */
-void _floatp(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-/**
- * _stringp - prints a string
- * @list: argument pointer variable, string to print
- */
-void _stringp(va_list list)
-{
-	char *s;
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
 
-	s = va_arg(list, char*);
-	if (s == NULL)
+/**
+* _strings - Print arguments of type date char.
+* @list: value vector arguments.
+*/
+
+void _strings(va_list list)
+{
+	char *str;
+
+	str = va_arg(list, char *);
+	if (str == NULL)
 	{
 		printf("(nil)");
 		return;
 	}
-		printf("%s", s);
+
+	printf("%s", str);
+}
+/**
+* _chart - Print arguments of type date char.
+* @list: value vector arguments.
+*/
+
+void _chart(va_list list)
+{
+
+	printf("%c", va_arg(list, int));
+}
+/**
+* _ints - Print arguments of type date int.
+* @list: value vector arguments.
+*/
+
+void _ints(va_list list)
+{
+
+	printf("%i", va_arg(list, int));
 }
 
+/**
+* _floats - Print arguments of type date float.
+* @list: value vector arguments.
+*/
+
+void _floats(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- */
-void print_all(const char * const format, ...)
+* print_all - Print arguments of the all type of date.
+* @format: this value of the format type
+*/
+
+void print_all(const char *const format, ...)
 {
 	data_t types[] = {
-		{"c", _charp},
-		{"i", _intp},
-		{"f", _floatp},
-		{"s", _stringp},
-		{NULL, NULL}
-	};
-	int i = 0;
-	int j = 0;
+		{"c", _chart},
+		{"i", _ints},
+		{"f", _floats},
+		{"s", _strings},
+		{NULL, NULL}};
+
+	int index = 0, indexb = 0;
 	va_list list;
-	char *separator = "";
+
+	char *separador = "";
 
 	va_start(list, format);
 
-	while (format != NULL && format[i])
+	while (format != NULL && format[index])
 	{
-		j = 0;
-		while (types[j].type)
+		while (types[indexb].type)
 		{
-			if (format[i] == *types[j].type)
+			if (format[index] == *types[indexb].type)
 			{
-				printf("%s", separator);
-				types[j].func(list);
-				separator = ", ";
+				printf("%s", separador);
+				types[indexb].func(list);
+				separador = ", ";
 			}
-			j++;
+			indexb++;
 		}
-		i++;
+		index++;
 	}
 	printf("\n");
 	va_end(list);
-
 }
