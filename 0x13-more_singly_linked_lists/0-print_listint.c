37bacedfbaa0f2d53cf62ad5a8f65a8717dd5a0f
DKBARB10 <jjuannca651@gmail.com>
2021-08-03 01:06:48 -0500

    0x13_Linked_List_Nodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - singly linked list
- * @h: pointer
- * 
- * Return: Display
+ * print_listint -  prints all the elements of a listint_t.
+ * @h: list pointer.
+ *
+ * Return: list elements
  */
 
 size_t print_listint(const listint_t *h)
 {
 	size_t display = 0;
 
-	if(h == NULL)
+	if (h == NULL)
 		return (0);
 	while (h != NULL)
 	{
