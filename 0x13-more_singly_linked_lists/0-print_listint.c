d87527f37b74711a69ec2bec1d3636dab245b6d7
DKBARB10 <84897255+DKBARB10@users.noreply.github.com>
2021-08-03 00:43:24 -0500

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,22 +11,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t display = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->n == 0)
-		{
-			printf("%d\n", h->n);
-		}
-
-		if (h->n != '\0')
-		{
-			printf("%d\n", h->n);
-		}
+		nodes++;
+		printf("%d\n", h->n);
 		h = h->next;
-		display++;
 	}
 
-	return (display);
+	return (nodes);
 }
