8783190b3b9398ce23b22ae8e116a755cf3d2712
DKBARB10 <jjuannca651@gmail.com>
2021-08-02 19:32:06 -0500

    0x13_Single_Linked_list_Easy_Printer

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+    size_t display = 0;
+
+    while (h != NULL)
+    {
+        if (h->n != '\0')
+        {
+            printf("%d\n", h->n);
+        }
+        h = h->next;
+        display++;
+    }
+
+    return (display);
+}
