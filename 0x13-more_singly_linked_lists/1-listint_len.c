ff6c676225a473337ab97b30c05ebd884a8f4b50
DKBARB10 <jjuannca651@gmail.com>
2021-08-02 20:00:30 -0500

    0x13_Single_Linked_list_Add_New_Node

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,21 +3,12 @@
 #include <stdio.h>
 #include "lists.h"
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
     size_t display = 0;
 
     while (h != NULL)
     {
-        if (h->n == 0)
-        {
-            printf("%d", h->n);
-        }
-
-        if (h->n != '\0')
-        {
-            printf("%d\n", h->n);
-        }
         h = h->next;
         display++;
     }
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *new_node = malloc(sizeof(listint_t));
+
+    if (head == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->n = n;
+    new_node->next = (*head);
+
+    (*head) = new_node;
+    return (new_node);
+}
