67387fd8dd10e6165dd0a8b392e5fd1a4ef579d0
DKBARB10 <jjuannca651@gmail.com>
2021-08-05 18:52:40 -0500

    0x14_Bit_Manipulation_Binary_to_Unsigned_int

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
