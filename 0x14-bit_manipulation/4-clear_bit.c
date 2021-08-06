4250d5caf49b98a1a6a3c3026b798abc3c525e0d
DKBARB10 <jjuannca651@gmail.com>
2021-08-05 23:59:32 -0500

    0x14_Bit_Manipulation_Gets_Bits

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - Gets the value of a bit at a given index.
+ * @n: the bit
+ * @index: The unsigned int of index
+ * Return: if an error occurs - -1
+ * otherwise -the value of bit at index
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n &= ~(1 << index);
+	return (1);
+}
