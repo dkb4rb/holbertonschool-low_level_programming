bb91abe56e287b32065a607473e34f4e97831601
DKBARB10 <jjuannca651@gmail.com>
2021-08-09 22:35:48 -0500

    0x15_File_Io_Read_W_O_R34dm3_P

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <limits.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
