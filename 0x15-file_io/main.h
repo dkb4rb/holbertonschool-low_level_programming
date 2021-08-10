4ca0d9e252faeca8d776193d8b954e62f91ec877
DKBARB10 <jjuannca651@gmail.com>
2021-08-09 22:38:10 -0500

    0x15_File_Io_Read_W_O_R34dm3_P

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdio.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <limits.h>
-
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-
-#endif /* MAIN_H */
+#endif /* HOLBERTON_H */
