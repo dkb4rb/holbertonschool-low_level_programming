0023e960de1ee8412150bdb623898f0010189ed1
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-10 10:48:18 -0500

    Hash Table BEtty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,22 +2,29 @@
 
 hash_table_t *hash_table_create(unsigned long int size);
 
+/**
+ * hash_table_create - Create new table of hashing
+ * @size: thi is size of the array of hash
+ * Return: new has table
+ **/
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_Table;
-    unsigned long int l;
+	hash_table_t *new_Table;
+	unsigned long int l;
 
-    new_Table = malloc(sizeof(hash_table_t));
-    if(new_Table == NULL )
-      return(NULL);
-	
-    new_Table->size = size;
+	new_Table = malloc(sizeof(hash_table_t));
+	if (new_Table == NULL)
+		return (NULL);
+
+	new_Table->size = size;
 	new_Table->array = malloc(sizeof(hash_node_t *) * size);
-    if(new_Table->array == NULL)
-      return (NULL);
-    while(l < size){
-      l++;
-      new_Table->array[l] = NULL;
-    }
-    return (new_Table);
+	if (new_Table->array == NULL)
+		return (NULL);
+	while (l < size)
+	{
+		l++;
+		new_Table->array[l] = NULL;
+	}
+	return (new_Table);
 }
