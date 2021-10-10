5f99a939366e1ce520795b2ec930993f31078dd2
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-10 10:39:04 -0500

    Create new Hash Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,6 +4,20 @@ hash_table_t *hash_table_create(unsigned long int size);
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+    hash_table_t *new_Table;
+    unsigned long int l;
 
-	return (0);
+    new_Table = malloc(sizeof(hash_table_t));
+    if(new_Table == NULL )
+      return(NULL);
+	
+    new_Table->size = size;
+	new_Table->array = malloc(sizeof(hash_node_t *) * size);
+    if(new_Table->array == NULL)
+      return (NULL);
+    while(l < size){
+      l++;
+      new_Table->array[l] = NULL;
+    }
+    return (new_Table);
 }
