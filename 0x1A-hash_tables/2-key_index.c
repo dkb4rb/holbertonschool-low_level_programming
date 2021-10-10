fe73f29302b423d50edaa9f4e749325bf3c4bf6e
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-10 15:29:05 -0500

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,13 +7,11 @@ unsigned long int key_index(const unsigned char *key,
  * key_index - function that gives index of a key
  * @key: is the key
  * @size: is te size of the array
- * Return: index of the key
+ * Return: The index of the key
 */
 
 unsigned long int key_index(const unsigned char *key,
 			    unsigned long int size)
 {
-	printf("%s\n\n", key);
-	printf("%ld", size);
-	return (1);
+	return (hash_djb2(key) % size);
 }
