c2916056870b54c39ece5eef72301a926af01b0f
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-10 14:31:06 -0500

    .

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -34,5 +35,6 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
