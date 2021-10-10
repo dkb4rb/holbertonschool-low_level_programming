e267dbbf152ea20c589bdd81b572e1705506d702
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-10 03:49:58 -0500

    .

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	return (0);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+0x1A-hash_tables 
+#Author Juan Duque
+Holberton School 2021 C15 mkdir tests
