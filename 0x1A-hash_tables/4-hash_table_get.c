dae83a28078c3331d87868df872edea6dbf69e39
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-12 08:06:33 -0500

    .

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,19 +15,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 
-    hash_node_t *node;
-    unsigned long int index;
+	hash_node_t *node;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    if (index >= ht->size)
-        return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
-    node = ht->array[index];
-    while (node && strcmp(node->key, key) != 0)
-        node = node->next;
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 
-    return ((node == NULL) ? NULL : node->value);
+	return ((node == NULL) ? NULL : node->value);
 }
