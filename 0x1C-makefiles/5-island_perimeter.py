f5f4fcc3ad1ad905c43d694655295799f0611f41
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2021-11-29 01:22:27 -0500

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,14 +12,14 @@ def island_perimeter(grid):
     """
     width = len(grid[0])
     height = len(grid)
-    cateto_0 = 0
+    edge = 0
     cateto_1 = 0
 
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
-                cateto_0 += 1
+                size += 1
                 if ((j > 0 and grid[i][j - 1] == 1) or (
                         i > 0 and grid[i - 1][j] == 1)):
-                    cateto_1 += 1
+                    edge += 1
     return (cateto_0 * 4) - (cateto_1 * 2)
